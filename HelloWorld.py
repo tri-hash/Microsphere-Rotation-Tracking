a92c7388b0991142a40b7fb5b180e318e1bbf4d9
Taryn Imamura <imamura.taryn@gmail.com>
2026-06-01 14:56:04 -0400

    created hello world file to set up git repo

---
+++ HelloWorld.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  1 14:47:18 2026
+
+@author: Taryn
+"""
+
+# Demo file for Spyder Tutorial
+# Hans Fangohr, University of Southampton, UK
+
+def hello():
+    """Print "Hello World" and return None."""
+    print("Hello World")
+
+# Main program starts here
+hello()
